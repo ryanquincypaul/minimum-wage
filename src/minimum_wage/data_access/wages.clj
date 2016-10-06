7730e655ceddc8239d6da56f603d083d18ab323b
ryanquincypaul <littlefoot.paul@gmail.com>
2016-10-05 22:40:37 -0700

    began making this actually restful according to HAL

--- src/minimum_wage/data_access/wages.clj
+++ src/minimum_wage/data_access/wages.clj
@@ -24,12 +24,11 @@
       (read-resource server-resource)
       (read-resource war-resource)))
 
-;;(def state-csv-filename "wage_data/state_minimum_wage.csv")
+;;dev resources are for testing locally or as a fallback in case the server doesn't have any csv files in resource folder
 (def state-csv-resource "wage_data/state_minimum_wage.csv")
 (def state-csv-resource-dev "wage_data/dev/state_minimum_wage.csv")
 (def state-wages-collection (parse (get-wage-data state-csv-resource state-csv-resource-dev)))
 
-;;(def federal-csv-filename "wage_data/federal_minimum_wage.csv")
 (def federal-csv-resource "wage_data/federal_minimum_wage.csv")
 (def federal-csv-resource-dev "wage_data/dev/federal_minimum_wage.csv")
 (def federal-wages-collection (parse (get-wage-data federal-csv-resource federal-csv-resource-dev)))
@@ -38,6 +37,25 @@
   "Get federam minimum-wage from federal wage csv by year."
   [year federal-wages]
   ((keyword (str year)) (first federal-wages)))
+
+(defn href-map
+  "create href map for link"
+  [uri]
+  {:href uri})
+
+(defn link-map
+  "take a key value pair and creates a link that will be returned in the links list"
+  [kvp]
+  {(key kvp) (href-map (val kvp))})
+
+(defn form-links
+  "form values for links section of responses"
+  ([links]
+    (form-links links []))
+  ([links output-map]
+    (if (empty? links)
+      output-map 
+      (recur (rest links) (conj output-map (link-map (first links)))))))
 
 (defn form-year-item
   "Create a map containing year and uri to get wage info."
@@ -50,7 +68,7 @@
    (form-get-years-response years []))
   ([years output-vector]
    (if (empty? years)
-     {:years output-vector}
+     {:links (form-links {:self "/years"}) :years output-vector}
      (recur (rest years) (conj output-vector (form-year-item (first years)))))))
 
 (defn get-years
@@ -61,28 +79,28 @@
    (let [year-keys (keys (dissoc (first state-wages) :state :postalcode))]
      (form-get-years-response year-keys))))
 
-(defn form-get-states-by-year-item
+(defn form-get-year-wage-info-state-item
   "create a map containing state, postalcode, and uri to get state wage"
   [year state-wage-map]
   (assoc
    (select-keys state-wage-map [:state :postalcode])
    :get-state-wage-info-for-year-uri (str "/" year "/" (:postalcode state-wage-map))))
 
-(defn form-get-states-by-year-response
+(defn form-get-year-wage-info-response
   "Returns filtered states into response"
   ([year state-wages federal-wages]
-   (form-get-states-by-year-response year state-wages [] federal-wages))
+   (form-get-year-wage-info-response year state-wages [] federal-wages))
   ([year state-wages output-vector federal-wages]
    (if (empty? state-wages)
      {(keyword (str year)) {:states output-vector :federal (get-federal-wage-by-year year federal-wages)}}
-     (recur year (rest state-wages) (conj output-vector (form-get-states-by-year-item year (first state-wages))) federal-wages))))
+     (recur year (rest state-wages) (conj output-vector (form-get-year-wage-info-state-item year (first state-wages))) federal-wages))))
 
-(defn get-states-by-year
+(defn get-year-wage-info
   "Returns the states available and the federal minimum wage for given year. Allows for sample state and federal wage data for testing."
   ([year]
-   (get-states-by-year year state-wages-collection federal-wages-collection))
+   (get-year-wage-info year state-wages-collection federal-wages-collection))
   ([year state-wages federal-wages]
-   (form-get-states-by-year-response
+   (form-get-year-wage-info-response
     year
     (filter #(not (nil? ((keyword (str year)) %))) state-wages)
     federal-wages)))
--- test/minimum_wage/wages_test.clj
+++ test/minimum_wage/wages_test.clj
@@ -12,11 +12,12 @@
   [{:2015 "7.25" :2016 "7.25" :2017 "7.25" :2018 "7.25"}])
 
 (deftest get-years-test
-  (let [response (get-years parsed-state-data)]
-    (is (= response {:years [{:year "2015" :get-states-by-year-uri "/2015"}
-                             {:year "2016" :get-states-by-year-uri "/2016"}
-                             {:year "2017" :get-states-by-year-uri "/2017"}
-                             {:year "2018" :get-states-by-year-uri "/2018"}]}))))
+  (let [response (get-years parsed-state-data parsed-federal-data)]
+    (is (= response {:_links {:self {:href "/years"}} 
+                     :years [{:_links {:self {:href "/year/2015"}} :year "2015" }
+                             {:_links {:self {:href "/year/2016"}} :year "2016" }
+                             {:_links {:self {:href "/year/2017"}} :year "2017" }
+                             {:_links {:self {:href "/year/2018"}} :year "2018" }]}))))
 
 (def ^{:private true} common-year
   2016)
@@ -24,25 +25,51 @@
 (deftest get-year-states-info-test
   ;Use year that all states have a value for
   (let [response (get-states-by-year common-year parsed-state-data parsed-federal-data)]
-    (is (= response {:2016 {:states [{:state "Alabama" :postalcode "AL" :get-state-wage-info-for-year-uri "/2016/AL"}
-                                     {:state "Massachusetts" :postalcode "MA" :get-state-wage-info-for-year-uri "/2016/MA"}
-                                     {:state "Michigan" :postalcode "MI" :get-state-wage-info-for-year-uri "/2016/MI"}]
-                            :federal "7.25"}}))))
+    (is (= response {:_links {:self {:href "/year/2016"}} 
+                     :year "2016"
+                     :states [{:_links {:self {:href "/year/2016/state/al"}} :state "Alabama" :postalcode "AL"}
+                              {:_links {:self {:href "/year/2016/state/ma"}} :state "Massachusetts" :postalcode "MA"}
+                              {:_links {:self {:href "/year/2016/state/mi"}} :state "Michigan" :postalcode "MI"}]
+                     :federal-wage "7.25"}))))
 
 ;to prevent responses of states with null or no values for a year
 (def ^{:private true} some-missing-year
   2018)
 
 (deftest get-year-wage-info-missing-test
-  (let [response (get-states-by-year some-missing-year parsed-state-data parsed-federal-data)]
-    (is (= response {:2018 {:states [{:state "Massachusetts" :postalcode "MA" :get-state-wage-info-for-year-uri "/2018/MA"}
-                                     {:state "Michigan" :postalcode "MI" :get-state-wage-info-for-year-uri "/2018/MI"}]
-                            :federal "7.25"}}))))
+  (let [response (get-year-wage-info some-missing-year parsed-state-data parsed-federal-data)]
+    (is (= response {:_links {:self {:href "/year/2018"}} 
+                     :year "2018" 
+                     :states [{:_links {:self {:href "/year/2018/state/ma"}} :state "Massachusetts" :postalcode "MA"}
+                              {:_links {:self {:href "/year/2018/state/mi"}} :state "Michigan" :postalcode "MI"}]
+                     :federal-wage "7.25"}))))
 
 (def ^{:private true} michigan-postal-code
   "mi")
 
 (deftest get-state-wage-info-test
   (let [response (get-state-wage-info-for-year common-year michigan-postal-code parsed-state-data parsed-federal-data)]
-    (is (= response {:2016 {:Michigan {:state "Michigan" :postalcode "MI" :wage "8.50"}
-                            :federal "7.25"}}))))
+    (is (= response {:_links {:self {:href "/year/2016/state/mi"}} 
+                     :year "2016" 
+                     :state "Michigan"
+                     :postalcode "MI"
+                     :wage "8.50"}))))
+
+(def ^{:private true} parsed-federal-data-extra-year
+  [{:2015 "7.25" :2016 "7.25" :2017 "7.25" :2018 "7.25" :2019 "15.00"}])
+
+(deftest get-years-with-only-federal-wage)
+  ;make sure we also return years even when we only have federal data
+  (let [response (get-years parsed-state-data parsed-federal-data-extra-year)]
+    (is (= response {:_links {:self {:href "/years"}} 
+                     :years [{:_links {:self {:href "/year/2015"}} :year "2015" }
+                             {:_links {:self {:href "/year/2016"}} :year "2016" }
+                             {:_links {:self {:href "/year/2017"}} :year "2017" }
+                             {:_links {:self {:href "/year/2018"}} :year "2018" }
+                             {:_links {:self {:href "/year/2019"}} :year "2019" }]})))
+
+(deftest get-year-wage-info-for-year-with-only-federal-data
+  (let [response (get-year-wage-info some-missing-year parsed-state-data parsed-federal-data-extra-year)]
+    (is (= response {:_links {:self {:href "/year/2019"}} 
+                     :year "2019" 
+                     :federal-wage "15.00"}))))
